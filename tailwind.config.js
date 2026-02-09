2816a7adcdaedff0edfd3a8c19845ff155d23056
azulviki <azulesvictoria@gmail.com>
2026-02-09 19:28:43 -0300

    arreglar tipografias

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@
 module.exports = {
   content: ["./*.html", "./js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'], // le damos un nombre "roboto"
+        onav: ['Michroma', 'sans-serif'], // le damos un nombre "onav"
+        open: ['Open Sans', 'sans-serif'], // le damos un nombre "open"
+    },
   },
   plugins: [],
 }
-
-
+}
